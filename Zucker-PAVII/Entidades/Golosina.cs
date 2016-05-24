53a4911f16d9546cbeb0f4e373241a6f420a6295
fbackhaus <fbackhaus94@gmail.com>
2016-05-24 12:21:59 -0300

    Agregando Entidad Golosina

---
+++ Zucker-PAVII/Entidades/Golosina.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Golosina
+    {
+        public int? id_golosina { get; set; }
+        public int id_marca { get; set; }
+        public int id_tipo_golosina { get; set; }
+        public string nombre { get; set; }
+        public string? descripcion { get; set; }
+        public int? stock { get; set; }
+        public float precio_vta { get; set; }
+        public bool es_propia { get; set; }
+        public int codigo_barras { get; set; }
+
+        public Golosina(int? dni, int id_marca, int id_tipo_golosina, int? stock, float precio_vta, bool es_propia, int codigo_barras)
+        {
+            this.id_golosina = id_golosina;
+            this.id_marca = id_marca;
+            this.id_tipo_golosina = id_tipo_golosina;
+            if (stock < 0)
+                this.stock = 0;
+            else
+                this.stock = stock;
+            this.precio_vta = precio_vta;
+            this.es_propia = es_propia;
+            this.codigo_barras = codigo_barras;
+
+        }
+
+    }
+}
