0e204c3f2a1390e7065f2a6e00afd233418274be
fbackhaus <fbackhaus94@gmail.com>
2016-05-24 18:57:51 -0300

    ABMC Parte de Guardar Lista

---
+++ Zucker-PAVII/Dao/GolosinaDao.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Dao
+{
+    public class GolosinaDao
+    {
+        public static void Insertar(Golosina gol)
+        {
+            //1. Abro la Conexion
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=FEDE-PC;Initial Catalog=BD_Golosinas;Integrated Security=True";
+            cn.Open();
+            //2.Creo el objeto command
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = @"Insert into Golosina (id_golosina,
+                                                    nombre,
+                                                    descripcion,
+                                                    id_marca,
+                                                    stock,
+                                                    id_tipo_golosina,
+                                                    precio_vta,
+                                                    es_propia,
+                                                    codigo_barras) values (
+                                                    @Id_Golosina,
+                                                    @Nombre,
+                                                    @Descripcion,
+                                                    @Id_Marca,
+                                                    @Stock,
+                                                    @Id_Tipo_Golosina,
+                                                    @Precio_Vta,
+                                                    @Es_Propia,
+                                                    @Codigo_Barras); select Scope_Identity() as ID";
+            cmd.Parameters.AddWithValue("@Id_Golosina", gol.id_golosina);
+            cmd.Parameters.AddWithValue("@Nombre", gol.nombre);
+            cmd.Parameters.AddWithValue("@Descripcion", gol.descripcion);
+            cmd.Parameters.AddWithValue("@Id_Marca", gol.id_marca);
+            cmd.Parameters.AddWithValue("@Stock", gol.stock);
+            cmd.Parameters.AddWithValue("@Id_Tipo_Golosina", gol.id_tipo_golosina);
+            cmd.Parameters.AddWithValue("@Precio_Vta", gol.precio_vta);
+            cmd.Parameters.AddWithValue("@Es_Propia", gol.es_propia);
+            cmd.Parameters.AddWithValue("@Codigo_Barras", gol.codigo_producto);
+
+            cmd.ExecuteNonQuery();
+
+            cn.Close();
+        }
+
+        public static int ultimoID()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString="Data Source=FEDE-PC;Initial Catalog=BD_Golosinas;Integrated Security=True";
+            cn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = "Select id from ID_GOLOSINA";
+            int r = Convert.ToInt32(cmd.ExecuteScalar());
+            cn.Close();
+            return r;
+        }
+        public static void actualizarID(int id_gol)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=FEDE-PC;Initial Catalog=BD_Golosinas;Integrated Security=True";
+            cn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = "Update ID_GOLOSINA set id = @id_gol";
+            cmd.Parameters.AddWithValue("@id_gol", id_gol);
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+
+        public static DataSet leerBD(string consulta)
+        {
+            
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=FEDE-PC;Initial Catalog=BD_Golosinas;Integrated Security=True";
+            cn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = consulta;
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            cn.Close();
+            return ds;
+        }
+
+    }
+}
--- Zucker-PAVII/Website/GolosinasWF.aspx.cs
+++ Zucker-PAVII/Website/GolosinasWF.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Entidades;
+using Dao;
 
 public partial class GolosinasWF : System.Web.UI.Page
 {
@@ -13,7 +14,12 @@ public partial class GolosinasWF : System.Web.UI.Page
         if(!IsPostBack)
         {
             btnEliminar.Enabled = false;
+            int idgol = GolosinaDao.ultimoID() + 1;
+            txtIdGolosina.Text = idgol.ToString();
+            cargarDDLTipoGolosina();
+            cargarDDLMarca();
         }
+
     }
     protected void btnGuardar_Click(object sender, EventArgs e)
     {
@@ -22,7 +28,14 @@ public partial class GolosinasWF : System.Web.UI.Page
         try
         {
             Golosina g = new Golosina();
-            g.id_golosina = int.Parse(txtIdGolosina.Text);
+     
+            int id_gol;
+                if (int.TryParse(txtIdGolosina.Text, out id_gol) == true)
+                {
+                    g.id_golosina = id_gol;
+                }
+
+
             
             g.nombre = txtNombre.Text;
             g.id_marca = ddlMarca.SelectedIndex;
@@ -44,10 +57,11 @@ public partial class GolosinasWF : System.Web.UI.Page
             }
             else
             {
-                //ACA AGREGAR EL GUARDAR DEL GOLOSINADAO
+                //GUARDO LA GOLOSINA EN LA BD
+               GolosinaDao.Insertar(g);
             }
             ID = g.id_golosina.Value;
-            txtDescripcion.Text = "ANDUVO PA";
+            GolosinaDao.actualizarID(g.id_golosina.Value);
             
         }
         catch(Exception ex)
@@ -62,6 +76,7 @@ public partial class GolosinasWF : System.Web.UI.Page
     }
     protected void btnNuevo_Click(object sender, EventArgs e)
     {
+
 
     }
     protected void btnEliminar_Click(object sender, EventArgs e)
@@ -82,4 +97,27 @@ public partial class GolosinasWF : System.Web.UI.Page
         }
         set { ViewState["ID"] = value; }
     }
+
+    protected void cargarDDLTipoGolosina()
+    {
+       string consultaTipo = "Select id_tipo_golosina, nombre FROM Tipo_Golosina";
+       ddlTipo.DataSource = GolosinaDao.leerBD(consultaTipo);
+       ddlTipo.DataTextField = "nombre";
+       ddlTipo.DataValueField = "id_tipo_golosina";
+       ddlTipo.DataBind();
+
+     ddlTipo.Items.Insert(0, new ListItem("Seleccione Tipo de Golosina", "0"));
+    }
+    protected void cargarDDLMarca()
+    {
+        string consultaMarca = "Select id_marca, nombre FROM Marca";
+        ddlMarca.DataSource = GolosinaDao.leerBD(consultaMarca);
+        ddlMarca.DataTextField = "nombre";
+        ddlMarca.DataValueField = "id_marca";
+        ddlMarca.DataBind();
+
+        ddlMarca.Items.Insert(0, new ListItem("Seleccione Marca", "0"));
+    }
+
+
 }
